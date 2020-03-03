5f9e65c889ed75689a351e81297a1c5ce206e05a
Nipun Batra and Shlevia Hotama <pair+nipun.batra+shelvia.hotama@thoughtworks.com>
2020-03-03 15:46:36 +0800

    [#24] Call drop column transformation from main entry point.

--- src/main/scala/dataguild/DataInjection.scala
+++ src/main/scala/dataguild/DataInjection.scala
@@ -3,6 +3,8 @@ package dataguild
 import org.apache.spark.sql.SparkSession
 
 object DataInjection {
+
+
   def main(args:Array[String]): Unit = {
 
     val spark = SparkSession
@@ -15,6 +17,24 @@ object DataInjection {
     sc.hadoopConfiguration.set("fs.s3a.access.key", args(0))
     sc.hadoopConfiguration.set("fs.s3a.secret.key", args(1))
 
-    WholeDataInjection.dataInjection(spark)
+    val airbnbDF = spark
+      .read
+      .format("csv")
+      .option("header", true)
+      .option("inferSchema", true)
+      .option("quote", "\"")
+      .option("escape", "\"")
+      .option("multiLine", true)
+      .load("src/main/resource/raw/listings.csv")
+
+    val columnsToRemove = Seq("listing_url", "scrape_id", "last_scraped", "name", "summary", "space",
+      "description", "neighborhood_overview", "notes", "transit", "access",
+      "interaction", "house_rules", "thumbnail_url", "medium_url",
+      "picture_url", "xl_picture_url", "host_id", "host_url", "host_name",
+      "host_about", "host_thumbnail_url", "host_picture_url", "country_code",
+      "country", "jurisdiction_names")
+
+    val airbnbColumnDroppedDF = DropColumnsTransformation.dropColumn(airbnbDF, columnsToRemove)
+
   }
 }
--- src/main/scala/dataguild/DropColumnsTransformation.scala
+++ src/main/scala/dataguild/DropColumnsTransformation.scala
@@ -1,41 +1,12 @@
 package dataguild
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame}
 
 object DropColumnsTransformation {
+
   def dropColumn(df: DataFrame, columns: Seq[String]): DataFrame = {
     val filteredDf = df.drop(columns: _*)
     filteredDf
-  }
-
-
-  def main(args: Array[String]): Unit = {
-
-    val columnsToRemove = Seq("listing_url", "scrape_id", "last_scraped", "name", "summary", "space",
-      "description", "neighborhood_overview", "notes", "transit", "access",
-      "interaction", "house_rules", "thumbnail_url", "medium_url",
-      "picture_url", "xl_picture_url", "host_id", "host_url", "host_name",
-      "host_about", "host_thumbnail_url", "host_picture_url", "country_code",
-      "country", "jurisdiction_names")
-
-    val spark = SparkSession
-      .builder()
-      .master("local")
-      .appName("spark drop columns transformation")
-      .getOrCreate()
-
-    val airbnbDF = spark
-      .read
-      .format("csv")
-      .option("header", true)
-      .option("inferSchema", true)
-      .option("quote", "\"")
-      .option("escape", "\"")
-      .option("multiLine", true)
-      .load("src/main/resource/raw/listings.csv")
-
-    val outputDF = dropColumn(airbnbDF, columnsToRemove)
-    outputDF.show()
   }
 
 }
