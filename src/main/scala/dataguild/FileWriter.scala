c4a6ba9e4a281f7a9de478562b475edbe8be55b8
Nipun Batra and Shlevia Hotama <pair+nipun.batra+shelvia.hotama@thoughtworks.com>
2020-03-06 18:16:53 +0800

    [#29] Update Schema casting to use Foldleft. Remove breaking changes

--- src/main/scala/dataguild/FileWriter.scala
+++ src/main/scala/dataguild/FileWriter.scala
@@ -3,19 +3,21 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object FileWriter {
 
+  val PARTITION_COLS = Seq("currentDate")
+
   def writeToError(df: DataFrame, format: String, spark: SparkSession): Unit ={
     val bucketPath = spark.conf.get("pathToSave")
-    write(df, format, bucketPath+"error")
+    write(df, format, bucketPath+"error", PARTITION_COLS)
   }
 
   def writeToRaw(df: DataFrame, format: String, spark: SparkSession): Unit ={
     val bucketPath = spark.conf.get("pathToSave")
-    write(df, format, bucketPath+"raw")
+    write(df, format, bucketPath+"raw",  PARTITION_COLS)
   }
 
   def writeToValid(df: DataFrame, format: String, spark: SparkSession): Unit ={
     val bucketPath = spark.conf.get("pathToSave")
-    write(df, format, bucketPath+"valid")
+    write(df, format, bucketPath+"valid",  PARTITION_COLS)
   }
 
   def write(df: DataFrame, format: String, folderPath: String, partitionCols: Seq[String] = Seq()): Unit = {
@@ -23,7 +25,7 @@ object FileWriter {
       .write
       .mode("append")
       .format(format)
-      .partitionBy("currentDate")
+      .partitionBy(partitionCols:_*)
       .save(folderPath)
   }
 }
